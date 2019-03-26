bac2c430a6bee4d05fe04a1a75b481d2f5701dda
Gustavo Matsuo <gustavomatsuof@gmail.com>
2019-03-25 23:17:43 -0300

    Logica da Calculadora01

--- ProjetoCalculadora/App.js
+++ ProjetoCalculadora/App.js
@@ -4,16 +4,39 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 import Button from './src/components/Button'
 import Display from './src/components/Display'
 
+const InitialState={
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  value: [0,0],
+  corrent: 0,
+}
+
 export default class App extends Component{
-  state={
-    DisplayValue:'0',
-  }
+  state={ ...InitialState }
 
   addDigi = n =>{
-    this.setState({DisplayValue:n})
+    //Evita que mais de um "." seja adicionado
+    if(n == '.' && this.state.displayValue.includes('.')){
+      return
+    }
+
+    //Testa se tiver apenas o "0" no display ele receber "True" (na realidade 0 deixando de ser "false")
+    //caso não seja "0" ele recebe "false" (ou melhor dizendo se mantendo com mesmo valor), para não limpar o display
+    clearDisplay = this.state.displayValue === '0'|| 
+      this.state.clearDisplay
+
+    //Testa se o clearDisplay for "0" o displayValue recebe "" limpando o display, caso não seja 
+    //ele será "false" o display mantem o que esta nela para adicionar o proximo numero
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+
+    //Adiciona um numero atras do outro
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false})
   }
   clearMemory = () =>{
-    this.setState({DisplayValue:"0"})
+    //reseta o state
+    this.setState({...InitialState})
   }
   setOperation = operation =>{
     
@@ -22,7 +45,7 @@ export default class App extends Component{
   render() {
     return (
       <View style={styles.container}>
-      <Display value={this.state.DisplayValue}/>
+      <Display value={this.state.displayValue}/>
         <View style={styles.buttons}>
           <Button label='ac' triple onClick={this.clearMemory}/>
           <Button label='/' operation onClick={this.setOperation}/>
