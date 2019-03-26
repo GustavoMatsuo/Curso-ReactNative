3f4f61a82c4f64b20b63567fb1aaeaaa91be38d8
Gustavo Matsuo <gustavomatsuof@gmail.com>
2019-03-26 16:43:25 -0300

    Logica da Calculadora02 e Correcao de erros

--- ProjetoCalculadora/App.js
+++ ProjetoCalculadora/App.js
@@ -8,8 +8,8 @@ const InitialState={
   displayValue: '0',
   clearDisplay: false,
   operation: null,
-  value: [0,0],
-  corrent: 0,
+  values: [0,0],
+  current: 0,
 }
 
 export default class App extends Component{
@@ -23,7 +23,7 @@ export default class App extends Component{
 
     //Testa se tiver apenas o "0" no display ele receber "True" (na realidade 0 deixando de ser "false")
     //caso não seja "0" ele recebe "false" (ou melhor dizendo se mantendo com mesmo valor), para não limpar o display
-    clearDisplay = this.state.displayValue === '0'|| 
+    const clearDisplay = this.state.displayValue === '0'|| 
       this.state.clearDisplay
 
     //Testa se o clearDisplay for "0" o displayValue recebe "" limpando o display, caso não seja 
@@ -33,13 +33,53 @@ export default class App extends Component{
     //Adiciona um numero atras do outro
     const displayValue = currentValue + n
     this.setState({ displayValue, clearDisplay: false})
+
+    if(n !== '.'){
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({ values })
+    }
   }
   clearMemory = () =>{
     //reseta o state
     this.setState({...InitialState})
   }
   setOperation = operation =>{
-    
+    //muda o current caso seja o primeiro numero que foi digitado
+    if(this.state.current === 0){
+      this.setState({operation, current: 1, clearDisplay: true})
+    }
+    //fara a operação e setar o current, operation e etc
+    else{
+      //caso operation seja "=" ira tornar true
+      const equals = operation === '='
+      const values = [...this.state.values]
+
+      try{
+        //o eval faz a conta
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      }
+      catch(e){
+        values[0] = this.state.values[0]
+      }
+
+      //limpa o value[1] para receber os proximos numeros
+      values[1] = 0
+      this.setState({
+        displayValue: values[0],
+        //testa se for true ele limpa a variavel, caso contrario ele 
+        //armazena para realizar a conta na proxima chamada
+        operation: equals ? null : operation, 
+        //testa se for true ele seta para o 0 para reiniciar toda operação,
+        //caso contrario ele seta para recber o proximo numero para ralizar a operação 
+        current: equals ? 0 : 1,
+        //se for true ele não limpara o display, caso contrario ele limpara para 
+        //receber o proximo numero para a operação
+        clearDisplay: !equals,
+        values,
+      })
+    }
   }
 
   render() {
