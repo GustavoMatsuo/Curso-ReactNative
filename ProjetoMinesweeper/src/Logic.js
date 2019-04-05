62d2535b05d8af3481ebd37a687fa17a9ac39432
Gustavo Matsuo <gustavomatsuof@gmail.com>
2019-04-05 17:35:45 -0300

    Logica02

--- ProjetoMinesweeper/src/Logic.js
+++ ProjetoMinesweeper/src/Logic.js
@@ -35,4 +35,66 @@ const createMinedBoard=(rows, columns, minedAmount)=>{
     return board
 }
 
-export {createMinedBoard}
+const cloneBoard= board => {
+    return board.map(row => {
+        return row.map(field => {
+            return {...field}
+        })
+    })
+}
+
+const getNeighbors = (board, row, column) =>{
+    const neighbors = []
+    const rows = [row - 1, row, row+1]
+    const columns = [column - 1, column, column + 1]
+    rows.forEach(r => {
+        columns.forEach(c => {
+            const different = r !== row || c !== column
+            const validRow = r >= 0 && r < board.length
+            const validColumn = c >= 0 && c < board[0].length
+            if(different && validRow && validColumn){
+                neighbors.push(board[r][c])
+            }
+        })
+    })
+    return neighbors
+}
+
+const safeNeighborhood = (board, row, column) => {
+   const safes = (result, neighbor) => result && !neighbor.mined
+   return getNeighbors(board, row, column).reduce(safes, true)
+}
+
+const openField = (board, row, column) => {
+    const field = board[row][column]
+    if(!field.opened){
+        field.opened = true
+        if(field.mined){
+            field.exploded = true
+        }
+        else if(safeNeighborhood(board, row, column)){
+            getNeighbors(board, row, column).forEach(n =>
+                openField(board, n.nRow, n.nColumn)
+                )
+        }
+        else{
+            const neighbors = getNeighbors(board, row, column)
+            field.nearMines = neighbors.filter(n => n.mined).length
+        }
+    }
+}
+
+const fields = board => [].concat(...board)
+const hadExplosion = board => fields(board).filter(field => field.exploded).length
+const pendding = field => (field.mined && !field.flagged) || (!field.mined && !field.opened)
+const wonGame = board => fileds(board).filter(pendding).length === 0
+const showMines = board => fields(board).filter(field => field.mined).forEach(filed => filed.opened = true)
+
+export {
+    createMinedBoard,
+    cloneBoard,
+    openField,
+    hadExplosion,
+    wonGame,
+    showMines
+}
