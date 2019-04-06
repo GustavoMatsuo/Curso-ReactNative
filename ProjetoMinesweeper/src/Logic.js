dd7e6366959b743b1321bb9569f303c542907947
Gustavo Matsuo <gustavomatsuof@gmail.com>
2019-04-06 17:48:25 -0300

    Logica03

--- ProjetoMinesweeper/src/Logic.js
+++ ProjetoMinesweeper/src/Logic.js
@@ -90,11 +90,17 @@ const pendding = field => (field.mined && !field.flagged) || (!field.mined && !f
 const wonGame = board => fields(board).filter(pendding).length === 0
 const showMines = board => fields(board).filter(field => field.mined).forEach(field => field.opened = true)
 
+const invertFlag = (board, row, column) =>{
+    const field = board[row][column]
+    field.flagged = !field.flagged
+}
+
 export {
     createMinedBoard,
     cloneBoard,
     openField,
     hadExplosion,
     wonGame,
-    showMines
+    showMines,
+    invertFlag
 }
