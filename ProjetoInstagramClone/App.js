f5667b4dde7a76db2d4f8e7c3d0ff02f32d4ac84
Gustavo Matsuo <gustavomatsuof@gmail.com>
2019-06-11 23:03:50 -0300

    Add Post

--- ProjetoInstagramClone/App.js
+++ ProjetoInstagramClone/App.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 import Header from './components/Header'
+import {View, Text} from 'react-native'
+import Post from './components/Post'
 
 export default class App extends Component{
     render() {
       return (
-        <Header />
+        <View style={{flex: 1}}>
+          <Header />
+          <Post image={require('./assets/imgs/fence.jpg')} />
+        </View>
+
       )
     }
 }
