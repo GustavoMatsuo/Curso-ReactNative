f16327a50c48faa5e3552b0fc6438156d1512f53
Gustavo Matsuo <gustavomatsuof@gmail.com>
2019-06-14 11:23:35 -0300

    Criação da TabBar e permissoes

---
+++ ProjetoInstagramClone/src/Navigator.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { createBottomTabNavigator } from 'react-navigation'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+import Feed from './screens/Feed'
+
+const MeuRoutes = {
+    Feed: {
+        name: 'Feed',
+        screen: Feed,
+        navigationOptions: {
+            title: 'Feed',
+            tabBarIcon: ({ tintColor }) =>
+                <Icon name='home' size={25} color={tintColor} />
+        }
+    },
+    Add: {
+        name: 'AddPhoto',
+        screen: Feed,
+        navigationOptions: {
+            title: 'AddPhoto',
+            tabBarIcon: ({ tintColor }) => 
+                <Icon name='camera' size={25} color={tintColor} />
+        }
+    },
+    Profile: {
+        name: 'Profile',
+        screen: Feed,
+        navigationOptions: {
+            title: 'Profile',
+            tabBarIcon: ({ tintColor }) => 
+                <Icon name='user' size={25} color={tintColor} />
+        }
+    }
+}
+
+const MenuConfig = {
+    initialRouteName: 'Feed',
+    tabBarOptions: {
+        showLabel: false
+    }
+}
+
+const MenuNavigator = createBottomTabNavigator(MeuRoutes, MenuConfig)
+
+export default MenuNavigator
