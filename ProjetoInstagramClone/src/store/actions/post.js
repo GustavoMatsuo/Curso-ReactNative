17eb810e9578b7170eccb2f3e584b102c5e03208
Gustavo Matsuo <gustavomatsuof@gmail.com>
2019-06-27 18:14:47 -0300

    Integrando o Storage

--- ProjetoInstagramClone/src/store/actions/post.js
+++ ProjetoInstagramClone/src/store/actions/post.js
@@ -3,12 +3,23 @@ import axios from 'axios'
 
 export const addPost = post => {
     return dispatch => {
-        axios.post('/post.json', {...post})
-            .then(res => console.log(res.data))
+        //https://us-central1-projeto-reactnative.cloudfunctions.net/uploadImage
+        axios({ 
+            url: 'uploadImage',
+            baseURL: 'https://us-central1-projeto-reactnative.cloudfunctions.net/',
+            method: 'post',
+            data: {
+                image: post.image.base64
+            }
+        })
             .catch(err => console.log(err))
+            .then(res => {
+                post.image = res.data.imageUrl 
+                axios.post('/post.json', {...post })
+                    .then(res => console.log(res.data))
+                    .catch(err => console.log(err))
+            })
     }
-    
-    
     //return {
     //    type: ADD_POST,
     //    payload: post
